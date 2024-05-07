4015a5fde1949eb1da13ebee0b95f30f3624ded2
jackhsieh <jackhsien2002@gmail.com>
2024-05-07 19:03:35 +0800

    lazy prim implementation

--- minimum_spanning_tree/mst.py
+++ minimum_spanning_tree/mst.py
@@ -22,3 +22,30 @@ class Kruskal:
 
     def edges(self):
         return self.queue
+
+class LazyPrim:
+    def mst(self, graph):
+        pq = []
+        mst = []
+        marked = [False]*graph.V()
+
+        self.visit(graph, 0)
+
+        while pq:
+            edge = heappop(pq)
+            u = edge.either()
+            v = edge.other(u)
+            if marked[u] and marked[v]:
+                continue
+            mst.append(edge)
+            if not marked[u]:
+                self.visit(graph, u, marked)
+            if not marked[v]:
+                self.visit(graph, v, marked)
+        return mst
+
+    def visit(self, graph, u, marked, heap):
+        marked[u] = True
+        for edge in graph.adj(u):
+            if not marked[edge.other(u)]:
+                heappush(edge, key=lambda edge: edge.w)
